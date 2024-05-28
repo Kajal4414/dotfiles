ccf10ada134de316a31aa1303b14ffc32549cf7e
Sakshi Agrawal <81718060+sakshiagrwal@users.noreply.github.com>
2024-05-28 18:44:42 +0530

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -2,12 +2,12 @@
 
 # System update
 sudo apt update && sudo apt upgrade -y
+sudo mkdir -p /var/lib/samba/usershares/ # https://askubuntu.com/questions/798928/nautilus-share-message-called-net-usershare-info-but-it-failed
 
 # Install required packages
 sudo apt install -y git-all
 git config --global user.name "Sakshi Aggarwal"
 git config --global user.email "81718060+sakshiagrwal@users.noreply.github.com"
-git config --global core.autocrlf false
 git config --global core.editor "xed --wait"
 
 # cd Downloads
@@ -33,21 +33,40 @@ sudo wget -q -O /usr/lib/firefox/distribution/policies.json https://raw.githubus
 # Set Firefox-Mod-Blur theme
 cd ~/.mozilla/firefox/*.default-release/ && rm -rf chrome/ && git clone --depth 1 https://github.com/datguypiko/Firefox-Mod-Blur chrome && cd chrome/ && rm -rf !(ASSETS|*.css) .[^.]*
 
-# Set wallpaper
+# Download wallpaper
 wget -q --show-progress -O Downloads/wallpaper.png https://raw.githubusercontent.com/JaKooLit/Wallpaper-Bank/main/wallpapers/anime-girl-abyss.png
-# gsettings set org.cinnamon.desktop.background picture-uri 'file:///Downloads/wallpaper.png' # Wallpaper
 
-# Set Orchis-theme
-sudo rm -rf .themes/Orchis-Dark/
-git clone --depth 1 https://github.com/vinceliuice/Orchis-theme
-cd Orchis-theme/ && ./install.sh --color dark --size standard --libadwaita --tweaks black && cd .. && rm -rf cd Orchis-theme
-# gsettings set org.cinnamon.desktop.interface gtk-theme 'Orchis-Dark' # Applications
+# Install Colloid-gtk-theme
+git clone --depth 1 https://github.com/vinceliuice/Colloid-gtk-theme && cd Colloid-gtk-theme/ && ./install.sh -u && rm -rf ~/.themes/Colloid*
+./install.sh --color dark --tweaks black rimless && cd .. && rm -rf Colloid-gtk-theme/
+# sudo ln -s ~/.themes/Colloid-Dark/ /usr/share/themes/Colloid-Dark
+# sudo unlink /usr/share/themes/Colloid-Dark
 
-# Set Tela-circle-icon-theme
-sudo rm -rf .local/share/icons/Tela-Circle/ .local/share/icons/Tela-Circle-dark/ .local/share/icons/Tela-Circle-light/
-git clone --depth 1 https://github.com/vinceliuice/Tela-circle-icon-theme
-cd Tela-circle-icon-theme/ && ./install.sh -n Tela-Circle && cd .. && rm -rf Tela-circle-icon-theme
-# gsettings set org.cinnamon.desktop.interface icon-theme 'Colloid-dark' # Icons
+# Install Colloid-icon-theme
+git clone --depth 1 https://github.com/vinceliuice/Colloid-icon-theme && cd Colloid-icon-theme/ && rm -rf ~/.local/share/icons/Colloid*
+./install.sh && cd .. && rm -rf Colloid-icon-theme/
+# sudo ln -s ~/.local/share/icons/Colloid/ /usr/share/icons/Colloid
+# sudo unlink /usr/share/icons/Colloid
+
+# sudo nemo /usr/share/applications/
+# /home/sakshi/.local/share/icons/Colloid/apps/scalable/protonvpn-gui.svg
+# /home/sakshi/.local/share/icons/Colloid/apps/scalable/pycharm.svg
+# /home/sakshi/.local/share/icons/Colloid/apps/scalable/idea.svg
+
+# Install Orchis-theme
+git clone --depth 1 https://github.com/vinceliuice/Orchis-theme && cd Orchis-theme/ && ./install.sh -u && sudo rm -rf /usr/share/themes/Orchis*
+sudo ./install.sh --dest /usr/share/themes/ --color dark --size standard --tweaks black macos && cd .. && rm -rf Orchis-theme/
+
+# Install Tela-circle-icon-theme
+git clone --depth 1 https://github.com/vinceliuice/Tela-circle-icon-theme && cd Tela-circle-icon-theme/ && sudo rm -rf /usr/share/icons/Tela*
+sudo ./install.sh -d /usr/share/icons/ -n Tela-Circle && cd .. && rm -rf Tela-circle-icon-theme/
+# 'sudo xed /usr/share/applications/protonvpn-app.desktop' And add 'Icon=/usr/share/icons/Tela-Circle/scalable/apps/protonvpn.svg'
+# 'sudo xed /usr/share/applications/jetbrains-pycharm-ce.desktop' And add 'Icon=/usr/share/icons/Tela-Circle/scalable/apps/pycharm.svg'
+# 'sudo xed /usr/share/applications/jetbrains-idea-ce.desktop' And add 'Icon=/usr/share/icons/Tela-Circle/scalable/apps/idea.svg'
+
+# Install WhiteSur-icon-theme
+git clone --depth 1 https://github.com/vinceliuice/WhiteSur-icon-theme && cd WhiteSur-icon-theme/ && rm -rf ~/.local/share/icons/WhiteSur*
+./install.sh && cd .. && rm -rf WhiteSur-icon-theme/
 
 # Set Capitaine-cursors-theme
 wget -q --show-progress https://github.com/sainnhe/capitaine-cursors/releases/download/r5/Linux.zip
